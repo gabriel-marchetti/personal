20c94ee5c2038c93b2580732c757dbfcc56c0fdf
gabriel@desktop <g251055@dac.unicamp.br>
2024-03-07 07:16:03 -0300

    owl fight

--- aula_unicamp/2024_1s/mc521/practice/hackerearth/Owl_Fight/code.cpp
+++ aula_unicamp/2024_1s/mc521/practice/hackerearth/Owl_Fight/code.cpp
@@ -1,46 +1,42 @@
 #include <bits/stdc++.h>
-#define MAXN 100010
 
 using namespace std;
-long long OWLS[MAXN], N, M, Q;
 
-long long max_force( long long v ){
-    while( OWLS[v] >= 0 )
-        v = OWLS[v];
-    return OWLS[v];
+void swap( int x, int y ){
+    
 }
 
-void insert_relation( long long read1, long long read2 ){
-    long long parent, force;
-}
+class UnionFind {
+    private:
+        vector<int> parent, rank, setSize;
+        int numSets;
+    public:
+        // Construtor
+        UnionFind( int N ){
+            parent.assign(N, 0);
+            for( int i{0}; i < N; i++ )
+                parent[i] = i;
+            rank.assign(N, 0);
+            setSize.assign(N, 1);
+            numSets = N;
+        }
 
-int main( ){
-    cin >> N >> M;
-    for(int i{1}; i <= N; i++)
-        OWLS[i] = -i;
-    long long force, parent, read1, read2;
-    for(int i{1}; i <= M; i++){
-        cin >> read1 >> read2;
-        insert_relation(read1, read2);
-    }
+        int findSet( int i ){
+            return ( parent[i] == i ) ? i : parent[i] = findSet(parent[i]);  
+        }
 
-    for(int i{1}; i <= N; i++)
-        cout << OWLS[i] << ' ';
-    cout << endl;
+        bool isSame( int i, int j ){
+            return findSet(i) == findSet(j);
+        }
 
-    cin >> Q;
-    long long number1, number2;
-    for(int i{0}; i < Q; i++){
-        cin >> read1 >> read2;
-        number1 = max_force(read1);
-        number2 = max_force(read2);
-        if( number1 == number2 )
-            cout << "TIE" << endl;
-        else if( number1 > number2 )
-            cout << read1 << endl;
-        else
-            cout << read2 << endl;
-    }
+        int numDisjointSets(){ return numSets; }
 
-    return 0;
+        int sizeOfSet( int i ){
+            return setSize[findSet(i)];
+        }
+
+        void UnionSet( int i, int j ){
+            if( isSame(i, j) ) return;
+
+        }
 }
