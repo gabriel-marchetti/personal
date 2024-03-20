fafa31530826c360a2e7babf9a9bd452dceb2663
FouTY <g251055@dac.unicamp.br>
2024-03-20 13:36:30 -0300

    p725 uva_judge

--- aula_unicamp/2024_1s/mc521/practice/uva_judge/p725/tentativa2.cpp
+++ aula_unicamp/2024_1s/mc521/practice/uva_judge/p725/tentativa2.cpp
@@ -9,7 +9,29 @@ int main(){
     std::cin >> N;
     while( N != 0 ){
         existSol = false;
-        alreadyUsed.reset();
+
+        int j_copy, i_copy, aux;
+        for(int j{01234}; j <= 98765 / N; j++){
+            alreadyUsed.reset();
+            j_copy = j;
+            i_copy = N * j_copy;
+            while( i_copy ){
+                aux = i_copy % 10;
+                i_copy = i_copy / 10;
+                alreadyUsed.set(aux);
+            }
+
+            while( j_copy ){
+                aux = j_copy % 10;
+                j_copy = j_copy / 10;
+                alreadyUsed.set(aux);
+            }
+
+            if( alreadyUsed == mustBe ){
+                existSol = true;
+                printf("%d / %d = %d\n", j * N, j, N);
+            }
+        }
 
         if( !existSol )
             printf("There are no solution for %d.\n", N);
